e817380be9dd7a44c0be5b40d62fa62930d3b112
Junaid Ahmad <junaidhamdard11@gmail.com>
2018-03-03 16:01:22 +0530

    Android Slide or Walkthrough

---
+++ app/src/main/java/com/example/junaid/androidslide/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.junaid.androidslide;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnSlider1, btnSlider2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // set onClick
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/junaid/androidslide/slide1/SliderAdapter.java
@@ -0,0 +1,95 @@
+package com.example.junaid.androidslide.slide1;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.junaid.androidslide.R;
+
+public class SliderAdapter extends PagerAdapter {
+
+    private Context context;
+    private LayoutInflater layoutInflater;
+
+//    private TextView slideHeading, slideDescription;
+//    private ImageView slide_imageView;
+
+
+    public SliderAdapter(Context context) {
+
+        this.context = context;
+    }
+
+    // img Array
+    public int[] image_slide ={
+            R.drawable.eat_icon,
+            R.drawable.group_11,
+            R.drawable.group_12
+    };
+
+    // heading Array
+    public String[] heading_slide ={
+            "EAT",
+            "SLEEP",
+            "CODE"
+    };
+
+    // description Array
+    public String[] description_slide ={
+            "Splash Screen like professional with Animation in Android Studio",
+            "Splash Screen with Transition Animation",
+            " simple way to create Splash Scree"
+    };
+
+
+
+
+    @Override
+    public int getCount() {
+
+        return heading_slide.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout, container,false);
+        container.addView(view);
+
+        ImageView slide_imageView = view.findViewById(R.id.imageView1);
+        TextView slideHeading = view.findViewById(R.id.tvHeading);
+        TextView  slideDescription = view.findViewById(R.id.tvDescription);
+
+        slide_imageView.setImageResource(image_slide[position]);
+        slideHeading.setText(heading_slide[position]);
+        slideDescription.setText(description_slide[position]);
+
+        return view;
+    }
+
+
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((RelativeLayout)object);
+    }
+
+//    @Override
+//    public void destroyItem(ViewGroup container, int position, Object object) {
+//        View view = (View) object;
+//        container.removeView(view);
+//    }
+
+}
+
+
